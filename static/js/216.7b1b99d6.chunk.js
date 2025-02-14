acac868d2fb314c357740e38b73c11b9e1d7bd34
aura80 <96388713+aura80@users.noreply.github.com>
2025-02-14 22:41:45 +0000

    Deploying to gh-pages from @ aura80/goit-react-hw-05-movies@4668c36d492c66b504dde49f92b97727a417d2f3 🚀

---
+++ static/js/216.7b1b99d6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[216],{216:function(e,t,r){r.r(t);var n=r(184);t.default=function(){return(0,n.jsx)("div",{children:(0,n.jsx)("h1",{children:"404 Not Found"})})}}}]);
+//# sourceMappingURL=216.7b1b99d6.chunk.js.map
